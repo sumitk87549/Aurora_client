3e54498ea9275020edfd7c6671375d0a6bfd2652
sumitk87549 <sumitk87549@gmail.com>
2026-01-05 13:21:01 +0530

    angular - spring connection fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://becomes-instead-cottage-charlotte.trycloudflare.com'
+  apiUrl: 'https://becomes-instead-cottage-charlotte.trycloudflare.com/api'
 };
